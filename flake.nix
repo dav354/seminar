5c42f6efe2eb20232e21366e247553a7864ae8da
david <david@glaesle.cloud>
2025-04-21 09:07:52 +0200

    add direnv

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Dev shell for the seminar";
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.ansible
+        pkgs.ansible-lint
+      ];
+    };
+  };
+}
